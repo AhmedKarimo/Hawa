44c7a01d0bfd7fa77bdd287ab5eb146f0d555841
AhmedKarimo <41973508+AhmedKarimo@users.noreply.github.com>
2022-03-01 00:13:09 +0200

    customized user model and hawauser

--- hawaapp/admin.py
+++ hawaapp/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Phone, User, PeriodDate, Examination, OprateExamination, Prediction, Diagnos, RiskCalculator, OprateRisk
+from .models import Phone, HawaUser, PeriodDate, Examination, OprateExamination, Prediction, Diagnos, RiskCalculator, OprateRisk
 # Register your models here.
 # admin.site.register(admin)
-admin.site.register(User)
+admin.site.register(HawaUser)
 admin.site.register(OprateExamination)
 admin.site.register(RiskCalculator)
 admin.site.register(Phone)
--- hawaapp/models.py
+++ hawaapp/models.py
@@ -1,7 +1,9 @@
+from email.policy import default
 from django.db import models
-from django.db.models.aggregates import Max
-from django.db.models.base import Model
-from django.db.models.fields import IntegerField
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from platformdirs import user_runtime_path
 
 # Create your models here.
 
@@ -15,26 +17,61 @@ class PeriodDate(models.Model):
     per_date = models.DateField()
 
 
-class User(models.Model):
-    pho_id = models.OneToOneField(
-        Phone, on_delete=models.CASCADE, unique=True, null=True)
-    per_date = models.ForeignKey(PeriodDate, on_delete=models.CASCADE)
+class CustomAccountManager(BaseUserManager):
 
-    us_firstname = models.CharField(max_length=50)
-    us_lastname = models.CharField(max_length=50)
-    us_email = models.CharField(max_length=50)
-    us_birthdate = models.DateField(max_length=50)
-    us_rathe = models.CharField(max_length=50)
+    def create_superuser(self, email, firstname, lastname, password, **other_fields):
+        other_fields.setdefault("is_staff", True)
+        other_fields.setdefault("is_superuser", True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True'
+            )
+        return self.create_user(email, firstname, lastname, password, **other_fields)
+
+    def create_user(self, email, firstname, lastname,  password, **other_fields):
+
+        if not email:
+            raise ValueError(_("You must provide an email address"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, firstname=firstname,
+                          lastname=lastname, **other_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class HawaUser(AbstractBaseUser, PermissionsMixin):
+    phone = models.OneToOneField(
+        Phone, on_delete=models.CASCADE, unique=True, null=True)
+    per_date = models.OneToOneField(
+        PeriodDate, on_delete=models.CASCADE, null=True)
+
+    firstname = models.CharField(max_length=500, blank=True)
+    lastname = models.CharField(max_length=500, blank=True)
+    email = models.EmailField(_('email address'), unique=True)
+    birthdate = models.DateField(max_length=500, null=True)
+    rathe = models.CharField(max_length=500)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['firstname', 'lastname']
 
 
 class Examination(models.Model):
-    us_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    us_id = models.ForeignKey(HawaUser, on_delete=models.CASCADE)
     ex_question = models.CharField(max_length=200)
     ex_result = models.CharField(max_length=200)
 
 
 class OprateExamination(models.Model):
-    us_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    us_id = models.ForeignKey(HawaUser, on_delete=models.CASCADE)
     ex_id = models.ForeignKey(Examination, on_delete=models.CASCADE)
 
     ope_ex_date = models.DateField()
@@ -42,7 +79,7 @@ class OprateExamination(models.Model):
 
 
 class Prediction(models.Model):
-    us_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    us_id = models.ForeignKey(HawaUser, on_delete=models.CASCADE)
 
     pred_feature1 = models.DecimalField(max_digits=20, decimal_places=10)
     pred_feature2 = models.DecimalField(max_digits=20, decimal_places=10)
@@ -58,21 +95,21 @@ class Prediction(models.Model):
 
 
 class Diagnos(models.Model):
-    us_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    us_id = models.ForeignKey(HawaUser, on_delete=models.CASCADE)
     pred_id = models.ForeignKey(Prediction, on_delete=models.CASCADE)
 
     diag_date = models.DateField()
-    diag_diagnosis = models.CharField(max_length=50)
+    diag_diagnosis = models.CharField(max_length=500)
 
 
 class RiskCalculator(models.Model):
-    us_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    us_id = models.ForeignKey(HawaUser, on_delete=models.CASCADE)
 
     risk_question = models.CharField(max_length=200)
 
 
 class OprateRisk(models.Model):
-    us_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    us_id = models.ForeignKey(HawaUser, on_delete=models.CASCADE)
     risk_id = models.ForeignKey(RiskCalculator, on_delete=models.CASCADE)
 
     ope_risk_date = models.DateField()
